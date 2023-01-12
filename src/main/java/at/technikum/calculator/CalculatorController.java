9165728061a14e2aabc94b901aeab0f537e667d7
Nico Lerchl <nico@lerchl.one>
2023-01-12 13:27:02 +0100

    feat: square endpoint with path param

--- src/main/java/at/technikum/calculator/CalculatorController.java
+++ src/main/java/at/technikum/calculator/CalculatorController.java
@@ -1,6 +1,9 @@
 package at.technikum.calculator;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CalculatorController {
@@ -8,5 +11,10 @@ public class CalculatorController {
     @GetMapping("/sum")
     public int sum(@RequestParam int a, @RequestParam int b) {
         return a + b;
+    }
+
+    @GetMapping("/square/{x}")
+    public int square(@PathVariable("x") int x) {
+        return (int) Math.pow(x, 2);
     }
 }
